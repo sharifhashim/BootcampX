53a88fd99572e0eb39e4df6a2c8d1a063a0ddf63
Hashim Sharif <sharif.hashim@gmail.com>
2021-11-25 00:36:45 +0000

    use node-postgres library to connect to db

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,9 +7,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
-const limit = process.argv[3];
-
 pool.connect()
 .then(() => {
   console.log('database connected');
@@ -19,15 +16,17 @@ pool.connect()
 });
 
 pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts on cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${cohort}%'
-  LIMIT ${limit || 5 };
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 })
 .catch(err => {
